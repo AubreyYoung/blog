9044f3be15e107952e7b6beaf3ecc1ea13c7aa1e
AubreyYoung <yangguang1900@163.com>
2019-03-11 10:50:28 +0800

    add coding

---
+++ python/PythonCoding/coding20190310/object.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+"""
+-------------------------------------------------
+@version: 0.1
+@Author:AubreyYoung
+@license: Apache Licence 
+@contact: yangguang1900@163.com
+@site: https://github.com/AubreyYoung/blog
+@software: PyCharm
+@file: object.py
+@Date:2019/3/10 19:06
+-------------------------------------------------
+"""
+
+IdName = 'Oracle'
+
+print(id(IdName))
+
+print(type(IdName))
+
+print(IdName)
---
+++ python/PythonCoding/coding20190311/oldboy_python_day12_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+"""
+-------------------------------------------------
+@version: 0.1
+@Author:AubreyYoung
+@license: Apache Licence 
+@contact: yangguang1900@163.com
+@site: https://github.com/AubreyYoung/blog
+@software: PyCharm
+@file: oldboy_python_day12_session.py.py
+@Date:2019/3/11 10:48
+-------------------------------------------------
+"""
+
