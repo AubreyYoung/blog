7bef2095f253374a015b04c7cb59dc156626fc63
AubreyYoung <yangguang1900@163.com>
2019-03-14 09:39:56 +0800

    add

--- python/PythonCoding/coding20190311/oldboy_python_day11_session.py
+++ python/PythonCoding/coding20190311/oldboy_python_day11_session.py
@@ -15,3 +15,61 @@
 -------------------------------------------------
 """
 
+import time
+# print(time.time())
+# time.sleep(5)
+# print('MySQL')
+# print('Aubrey')
+
+def  func():
+    start = time.time()
+    print('Oracle')
+    end = time.time()
+    print(end - start)
+
+func()
+
+import time
+
+def timmer():
+    start = time.time()
+    func()
+    end = time.time()
+    print(end - start)
+
+def func():
+    print('Oracle')
+
+timmer()
+
+import time
+def timmer(f):
+    start = time.time()
+    f()
+    end = time.time()
+    print(end - start)
+
+def func():
+    print('Oracle')
+
+timmer(func)
+
+import time
+@timmer  # 语法糖
+def timmer(f):  # 装饰器函数
+    def inner():
+        start = time.time()
+        ret = f()  # 被装饰的函数
+        end = time.time()
+        print(end - start)
+        return  ret
+    return  inner
+
+def func():
+    print('Oracle')
+    return  'Hello'
+
+# func = timmer(func)
+ret = func()
+print(ret)
+
