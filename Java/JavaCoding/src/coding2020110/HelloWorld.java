7d3abf0eecea57a392e7ba7e5046704cd64d6c17
AubreyYoung <yangguang1900@163.com>
2020-01-10 16:37:08 -0500

    :memo:

---
+++ Java/JavaCoding/src/coding2020110/HelloWorld.java
@@ -0,0 +1,10 @@
+package coding2020110;
+
+/**
+ * Created by yangguang02 on 2020/1/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
